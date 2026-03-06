2e4676bc4a5dbbf241f1cbad8a1d58704fe85747
George Joel Concepcion Lopez <georgejoelconcepcionlopez@gmail.com>
2026-03-06 11:15:22 -0400

    prepare cloudflare deployment

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 console.log("[Vytrixe Deployment Monitor] Next.js Initialization Started. Verifying environment configurations...");
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   images: {
     remotePatterns: [
       {
